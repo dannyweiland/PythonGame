38987fec3969e0ae5ed6a2adaab2be5ed184b368
Daniel Weiland <49922377+dannyweiland@users.noreply.github.com>
2021-02-03 21:32:30 -0800

    Messing around with Pygame

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SpaceGame
- Messing around with pygame
+
+I've been trying to improve my knowledge in Python, so I decided to work on my skills by making a 'Space Invaders' style game using Pygame.  
+
+<div>Player icon by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+
+<div>Game Window icon made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
--- main.py
+++ main.py
@@ -1,28 +1,41 @@
 import pygame
 
-#Initialize the game
+# Initialize the game
 pygame.init()
 
-#Create the screen
+# Create the screen
 screen = pygame.display.set_mode((800, 800))
 
-#Create background
+# Create background
 background = pygame.image.load("intbuilding7a.png")
 
-#Title and Icon
+# Title and Icon
 pygame.display.set_caption("Danny's Video Game")
 icon = pygame.image.load('dog.png')
 pygame.display.set_icon(icon)
 
-#Game Function
+# Player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
+# Game Loop
 running = True
 while running:
+
+    # RGB Values for screen background
+    screen.fill((64, 35, 82))
+
+
     for event in pygame.event.get() :
         if event.type == pygame.QUIT :
             running = False
 
-    #RGB Values for screen background
-    screen.fill((0, 150, 0))
+    # Add Player Icon
+    player()
     pygame.display.update()
 
 
